3f0dc7ff1808429631ac5eb2a7483e807654090a
Joao Andrade <jpandrade.dev@gmail.com>
2021-06-29 16:50:36 +0200

    Last Push Charleroi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,155 +1,108 @@
-/*//create array of words
-var words = ["javascript", "html", "github", "google"]
 
-//pick a ramdom word
-var ramdomWord = words[Math.floor(Math.ramdom() * words.length)];
 
-//show how many letters are in the word
-var answerArray = [];
-for (var i = 0; i < words.length; i++) {
-    answerArray[i] = "_";
+var Champions = ["AATROX","AHRI","AKALI","ALISTAR","AMUMU","ANIVIA","ANNIE","APHELIOS","ASHE","AURELION SOL","AZIR","BARD","BLITZCRANK","BRAND","BRAUM","CAITLYN","CAMILLE","CASSIOPEIA","CHO GATH","CORKI","DARIUS","DIANA","DR MUNDO","DRAVEN","EKKO","ELISE","EVELYNN","EZREAL","FIDDLESTICKS","FIORA","FIZZ","GALIO","GANGPLANK","GAREN","GNAR","GRAGAS","GRAVES","HECARIM","HEIMERDINGER","ILLAOI","IRELIA","IVERN","JANNA","JARVAN IV","JAX","JAYCE","JHIN","JINX","KAI SA","KALISTA","KARMA","KARTHUS","KASSADIN","KATARINA","KAYLE","KAYN","KENNEN","KHA ZIX","KINDRED","KLED","KOG MAW","LEBLANC","LEE SIN","LEONA","LILLIA","LISSANDRA","LUCIAN","LULU","LUX","MALPHITE","MALZAHAR","MAOKAI","MASTER YI","MISS FORTUNE","MORDEKAISER","MORGANA","NAMI","NASUS","NAUTILUS","NEEKO","NIDALEE","NOCTURNE","NUNU AND WILLUMP","OLAF","ORIANNA","ORNN","PANTHEON","POPPY","PYKE","QIYANA","QUINN","RAKAN","RAMMUS","REK SAI","RELL","RENEKTON","RENGAR","RIVEN","RUMBLE","RYZE","SAMIRA","SEJUANI","SENNA","SERAPHINE","SETT","SHACO","SHEN","SHYVANA","SINGED","SION","SIVIR","SKARNER","SONA","SORAKA","SWAIN","SYLAS","SYNDRA","TAHM KENCH","TALIYAH","TALON","TARIC","TEEMO","THRESH","TRISTANA","TRUNDLE","TRYNDAMERE","TWISTED FATE","TWITCH","UDYR","URGOT","VARUS","VAYNE","VEIGAR","VEL KOZ","VI","VIKTOR","VLADIMIR","VOLIBEAR","WARWICK","WUKONG","XAYAH","XERATH","XIN ZHAO","YASUO","YONE","YORICK","YUUMI","ZAC","ZED","ZIGGS","ZILEAN","ZOE","ZYRA"];
+//Array with possible words
+
+var WordToGuess = Champions[Math.floor(Math.random()*Champions.length)];
+//Random word from array = word to guess
+
+var LettersToGuess = WordToGuess.split('');
+//Word to guess split into array of letters
+
+var CurrentLetters = [];
+//Array of letters showing current right and unknown letters
+
+var CurrentWord = "";
+//Actual state of the word to guess (with space to isolate letters)
+
+function ChangeCurrentWord() {
+    CurrentWord = "";
+    for (x=0;x<CurrentLetters.length;x++){
+        CurrentWord = CurrentWord + CurrentLetters[x] + " ";
+    }
+}
+//Function to update Current Word as Current Letters change
+
+var WrongLetters = [];
+//Letters previously guessed but incorrect
+
+var Wrong = "";
+//Wrong letters, for displaying purposes
+
+function ChangeWrong(){
+    Wrong = "";
+    for (x=0; x<WrongLetters.length;x++){
+        Wrong = Wrong + WrongLetters[x] + " ";
+    }
 }
 
-//how many letters are left
-var remainingLetters = word.length;
+//Below is the code that makes the game work
+for (x=0; x<LettersToGuess.length; x++){
+    if (LettersToGuess[x] == " "){
+        CurrentLetters.push (" ");
+    }
+    else {
+        CurrentLetters.push ("_");
+    }
+}
+//Create starting point: current letters are unknown, all replaced by underscores.
+
+ChangeCurrentWord();
+//Update Current state of guess to starting point
+
+document.getElementById("RightLetters").innerHTML = "The word is : " + CurrentWord;
+//Display number of letters to guess
 
 
-//MAIN CODE//
+console.log(CurrentWord);
+console.log(LettersToGuess);
+//debugging
 
-while (remainingLetters > 0) {
-    //show the progress
-    alert (answerArray.join(" "));
-
-    //get guess
-    var guess = prompt("Guess a letter, or click cancel to give up and stop playing.")
-
-    //if the guess is blank
-    if (guess == null) {
-        break;
-    
-    //if the guess is more than one letter or no letters
-    } else if (guess.length !== 1) {
-        alert("Please enter one letter at a time");
-    // see if the letter is in the array
-    } else {
-        for (var j = 0; j < word.length; j++) {
-            //if the letter they guesses is in the word
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-                //subtract one letter from the remaining letters
-                remainingLetters--;
-                }
+document.getElementById("try").addEventListener("click", () =>{
+//create click function to check letter currently being guessed  
+    if (WordToGuess.includes(document.getElementById("InputText").value.toUpperCase())){
+    //When the word has the letter the player guessed
+        for (i = 0; i < LettersToGuess.length; i++){ 
+            if (LettersToGuess[i] === document.getElementById("InputText").value.toUpperCase()) {  
+                CurrentLetters[i] = document.getElementById("InputText").value.toUpperCase();
+                ChangeCurrentWord();
+                
             }
+            // add newly guessed letter to current state of guessed word
+        };
+        console.log(CurrentWord);
+        console.log(CurrentLetters);
+        console.log(LettersToGuess);
+        //debugging
+        document.getElementById("RightLetters").innerHTML = CurrentWord;
+        // update state of word to guess
+          
+    }
+
+    else {
+    // if the word doesn't have the guessed letter
+        WrongLetters.push(document.getElementById("InputText").value.toUpperCase());
+        // add guessed letter to list of mistakes
+        ChangeWrong();
+        //update list of mistakes
+        document.getElementById("WrongLetters").innerHTML = "You already tried : " + Wrong;
+        // update list of mistakes shown on screen
+
+        if (WrongLetters.length == 6) {
+            alert("You lose!");
         }
+        //display an image according to number of wrong tries and alert lose when lost
     }
 
-//let player know the secret word
-alert(answerArray.join(" "));
+    for (i = 0; i < LettersToGuess.length; i++) {
+		if (CurrentLetters[i] !== LettersToGuess[i]) {
+            break;
+        }
+        else {
+            alert("You win!");
+        }
+	}  
+    //DOESNT WORK: win if one letter correct. Check if won (if any letter doesn't match, no loop. if last letter matches, win.)
 
-//congratulate
-alert("Good Job! The secret word was " + word);*/
-
-
-/*document.getElementById("button").addEventListener('click', () => {
-switch (new letter()) {
-    case  "A":
-        lettersInput = "A";
-        console.log("A");
-        break;
-    case  "B":
-        lettersInput = "B";
-        console.log("B");
-        break;          
-    }
-
-});*/
-[...document.getElementsByTagName("button")].forEach(function(item) {
-    // adding eventListener to the elements
-    item.addEventListener('click', function() {
-      // calling the methods
-      // this.id will be the id of the clicked button
-      // there is a method in the object by same name, which will be trigger
-      obj[this.id]();
-    })
-  })
-
-var obj = {
-    A: function() {
-        console.log('A')
-    },
-    B: function() {
-        console.log('B')
-    },
-    C: function() {
-        console.log('C')
-    },
-    D: function() {
-        console.log('D')
-    },
-    E: function() {
-        console.log('E')
-    },
-    F: function() {
-        console.log('F')
-    },
-    G: function() {
-        console.log('G')
-    },
-    H: function() {
-        console.log('H')
-    },
-    I: function() {
-        console.log('I')
-    },
-    J: function() {
-        console.log('J')
-    },
-    K: function() {
-        console.log('K')
-    },
-    L: function() {
-        console.log('L')
-    },
-    M: function() {
-        console.log('M')
-    },
-    N: function() {
-        console.log('N')
-    },
-    O: function() {
-        console.log('O')
-    },
-    P: function() {
-        console.log('P')
-    },
-    Q: function() {
-        console.log('Q')
-    },
-    R: function() {
-        console.log('R')
-    },
-    S: function() {
-        console.log('S')
-    },
-    T: function() {
-        console.log('T')
-    },
-    U: function() {
-        console.log('U')
-    },
-    V: function() {
-        console.log('V')
-    },
-    W: function() {
-        console.log('W')
-    },
-    X: function() {
-        console.log('X')
-    },
-    Y: function() {
-        console.log('Y')
-    },
-    Z: function() {
-        console.log('Z')
-    }
-}
+});
+// Need to add hangman, message when succeeded, message when losing, update readme, add colors and images
